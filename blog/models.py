b7cfb666423490170ef7abb78da82774f86188f5
Guillermo Ramirez <prograx@mesoamericana.edu.gt>
2021-11-03 19:55:37 -0600

    cambie el tamaño del texto

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 class Publicacion(models.Model):
     autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    titulo = models.CharField(max_length=100)
+    titulo = models.CharField(max_length=50)
     texto = models.TextField()
     fecha_creacion = models.DateTimeField(verbose_name='Creado',
             default=timezone.now)
