de24353a926168835c9695a357698171488e67e3
Guillermo Ramirez <prograx@mesoamericana.edu.gt>
2021-11-03 19:53:24 -0600

    Primera prueba de actions

--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,9 @@ from django.utils import timezone
 
 class Publicacion(models.Model):
     autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    titulo = models.CharField(max_length=200)
+    titulo = models.CharField(max_length=100)
     texto = models.TextField()
-    fecha_creacion = models.DateTimeField(
+    fecha_creacion = models.DateTimeField(verbose_name='Creado',
             default=timezone.now)
     fecha_publicacion = models.DateTimeField(
             blank=True, null=True)
